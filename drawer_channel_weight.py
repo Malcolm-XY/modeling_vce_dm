b89ce9b0a07f643ee8d88e6f396e323921dc79cc
Malcolm-XY <m18307300600@gmail.com>
2025-10-28 18:17:10 +0900

    20251028

--- drawer_channel_weight.py
+++ drawer_channel_weight.py
@@ -12,19 +12,19 @@ import matplotlib.pyplot as plt
 
 from utils import utils_feature_loading
 
-def get_channel_importance(folder='channel_importances', excel='channel_importances_LD.xlsx', sheet='label_driven_mi_1_5'):
+def get_channel_importances(folder='channel_importances', excel='channel_importances_LD.xlsx', sheet='label_driven_mi_1_5'):
     # define path
     path_current = os.getcwd()
 
     path_excel = os.path.join(path_current, folder, excel)
 
     # read xlxs; channel importance
-    importance = pd.read_excel(path_excel, sheet_name=sheet, engine='openpyxl')
-    importance = importance['ams']
+    importances = pd.read_excel(path_excel, sheet_name=sheet, engine='openpyxl')
+    importances = importances['ams']
 
-    return importance
+    return importances
 
-def rank_channel_strength(node_strengths, electrode_labels, ascending=False, exclude_electrodes=None):
+def rank_channel_importances(node_strengths, electrode_labels, ascending=False, exclude_electrodes=None):
     """
     Sort and visualize node strengths in a functional connectivity (FC) network,
     with optional electrode exclusion after sorting.
@@ -79,25 +79,22 @@ def rank_channel_strength(node_strengths, electrode_labels, ascending=False, exc
 
     return df_original, df_ranked, sorted_indices
 
-def draw_weight_map_from_file(ranking_method='label_driven_mi', offset=0, transformation='log', reverse=False):
+def draw_importance_map_from_file(ranking_method='label_driven_mi_1_5', offset=0, transformation='log', reverse=False):
     # 获取数据
-    index = get_index(ranking_method)
-    weights = get_ranking_weight(ranking_method)  # 假设它返回一个与 electrodes 对应的值列表
+    importances = get_channel_importances(sheet=ranking_method)
     if reverse:
-        weights = 1 - weights
+        importances = 1 - importances
     distribution = utils_feature_loading.read_distribution('seed')
 
-    dis_t = distribution.iloc[index]
-
-    x = np.array(dis_t['x'])
-    y = np.array(dis_t['y'])
-    electrodes = dis_t['channel']
+    x = np.array(distribution['x'])
+    y = np.array(distribution['y'])
+    electrodes = distribution['channel']
 
     # 归一化 label_driven_mi_mean 以适应颜色显示（假设它是数值列表）
     if transformation == 'log':
-        values = np.array(np.log(weights) + offset)
+        values = np.array(np.log(importances) + offset)
     else:
-        values = np.array(weights + offset)
+        values = np.array(importances + offset)
 
     # 绘制散点图
     plt.figure(figsize=(8, 6))
@@ -121,22 +118,20 @@ def draw_weight_map_from_file(ranking_method='label_driven_mi', offset=0, transf
 
     return None
 
-def draw_weight_map_from_data(index, weights, ranked_electrodes=None, offset=0, transformation=None, reverse=False):
+def draw_importance_map_from_data(importances, offset=0, transformation=None, reverse=False):
     if reverse:
-        weights = 1 - weights
+        importances = 1 - importances
     distribution = utils_feature_loading.read_distribution('seed')
 
-    dis_t = distribution.iloc[index]
-
-    x = np.array(dis_t['x'])
-    y = np.array(dis_t['y'])
-    electrodes = dis_t['channel']
+    x = np.array(distribution['x'])
+    y = np.array(distribution['y'])
+    electrodes = distribution['channel']
 
     # 归一化 label_driven_mi_mean 以适应颜色显示（假设它是数值列表）
     if transformation == 'log':
-        values = np.array(np.log(weights) + offset)
+        values = np.array(np.log(importances) + offset)
     else:
-        values = np.array(weights + offset)
+        values = np.array(importances + offset)
 
     # 绘制散点图
     plt.figure(figsize=(8, 6))
@@ -159,51 +154,15 @@ def draw_weight_map_from_data(index, weights, ranked_electrodes=None, offset=0, 
     plt.show()
 
     return None
-
-def draw_2d_mapping(am, coordinates, text, title='title'):
-    x = np.array(coordinates['x'])
-    y = np.array(coordinates['y'])
-    
-    # 点大小根据am缩放，避免太小或太大
-    size = 100 * (am - am.min()) / (am.max() - am.min() + 1e-8) + 30
-    
-    # 绘制散点图
-    plt.figure(figsize=(8, 6))
-    scatter = plt.scatter(x, y, c=am, cmap='coolwarm', s=100, edgecolors='k')
-    
-    # 添加颜色条
-    cbar = plt.colorbar(scatter)
-    cbar.set_label('Amplitude/Strength')
-    
-    # 添加文字标签
-    for i, txt in enumerate(text):
-        plt.text(x[i], y[i], txt, fontsize=9, ha='right', va='bottom')
-    
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    if title == None:
-        plt.title('Heatmap with Mapping')
-    else: 
-        plt.title(title)
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
 
 if __name__ == '__main__':
-    # label-driven-MI
-    # weights, index = draw_weight_map_from_file(ranking_method='label_driven_mi')
-
-    # electrodes = utils_feature_loading.read_distribution('seed')['channel']
-    # weight_mean_r = weights[index]
-    # from utils import utils_visualization
-
-    # utils_visualization.draw_heatmap_1d(weight_mean_r, electrodes)
-
-    # # data-driven-MI
-    # draw_weight_map_from_file(transformation=None, ranking_method='data_driven_mi')
-
-    # draw_weight_map_from_file(transformation=None, ranking_method='data_driven_pcc')
-
-    # draw_weight_map_from_file(transformation=None, ranking_method='data_driven_plv')
+    draw_importance_map_from_file(transformation=None, ranking_method='label_driven_mi_1_5')
+    draw_importance_map_from_file(transformation=None, ranking_method='label_driven_mi_10_15')
     
-    channel_importance = get_channel_importance(sheet='label_driven_mi_1_5')
+    channel_importances = get_channel_importances(sheet='label_driven_mi_1_5')
+    draw_importance_map_from_data(channel_importances)
+    channel_importances = get_channel_importances(sheet='label_driven_mi_10_15')
+    draw_importance_map_from_data(channel_importances)
+    
+    electrodes = utils_feature_loading.read_distribution('seed')['channel']
+    df_original, df_ranked, sorted_indices = rank_channel_importances(channel_importances, electrodes)
